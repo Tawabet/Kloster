10fec7d2aa847c2af7753c323842d2286d5842d7
Tawabet <81372879+Tawabet@users.noreply.github.com>
2021-05-06 15:06:47 +0000

    Deploying to gh-pages from master @ e5e47b4bac0b2cd83c6c8750daf9d2dd82570dbd 🚀

---
+++ wand-vertikal.tsx
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="wand-vertikal" tilewidth="32" tileheight="32" tilecount="2" columns="1">
+ <properties>
+  <property name="collides" type="bool" value="true"/>
+ </properties>
+ <image source="wand-vertikal.jpg" width="33" height="66"/>
+ <tile id="0">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
